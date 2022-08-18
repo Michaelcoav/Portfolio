31257769b2c85d9e30686b279da995e932bde7e2
Michaelcoav <michaelcoav@gmail.com>
2022-08-18 19:11:06 -0400

    Add project descriptions and made projects section responsive

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,18 +4,21 @@ const Project = ({img, alt, desc}) => {
     const projectProps = {
         width: "15rem",
         height: "15rem",
+        margin: "1em",
         borderRadius: "10%"
     };
 
 
     return (
-        <div className="row">
-            <div className="col" style={projectProps}>
-                <img src={img} alt={alt} />
+        <div className="row justify-content-center align-items-center">
+            <div className="col-md-5 col-sm-6 col d-flex justify-content-center">
+                <img src={img} alt={alt} style={projectProps}/>
             </div>
-            <div className="col">
-                <p>{desc}</p>
+            <div className="col-md-5 col-12">
+                <p className="text-center">{desc}</p>
             </div>
         </div>
     );
 };
+
+export default Project;
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -2,12 +2,22 @@ import accountManager from "../assets/AccountManager.png";
 import calculator from "../assets/Calculator.png";
 import ticTacToe from "../assets/TicTacToe.png";
 import toDo from "../assets/ToDoApp.png";
+import Project from "../components/Project";
 import { v4 as uuidv4 } from 'uuid';
 
 const Projects = () => {
+    // descriptions for all my projects
+    const toDoDesc = "The to do app was built using react, and the material mui library. The to do app allows the creation of tasks and the deletion of those tasks. This project helped me feel more confident in using states and creating components. It also helped me plan out how to organize different components of the project."
+    const accDesc = "The account manager similar to the one made by google that used Python module tkinter, to build the graphical user interface of the program, and SQLite module to store the name of the account, the username and password on a relational table. It was my first project outside of school where I got to learn about using different python modules.";
+    const calcDesc = "The calculator was built again using the Python module tkinter, to build the graphical user interface of the program, and python was used to create the back end functions. The calculator allows the user to perform addition, subtraction, multiplication, and division. It allows you to change the sign of the numbers, add decimals, and delete the most recent number, decimal, or operator from the calculator. It was my second project using tkinter and I felt much more comfortable building it.";
+    const ticDesc = "Tic-Tac-Toe was built using the Python module PyGame, to build buttons, draw the shapes, and print the text onto the screen. This was one of my favorite projects because it was very simple and I got to learn about the PyGame module.";
+
     // list of all my projects with img src
     const projects = [
-        {}
+        {src: toDo, alt: "ToDo App Project", desc: toDoDesc},
+        {src: accountManager, alt: "Account Manager Project", desc: accDesc},
+        {src: calculator, alt: "Calculator Project", desc: calcDesc},
+        {src: ticTacToe, alt: "Account Manager Project", desc: ticDesc},
     ];
 
 
@@ -18,6 +28,10 @@ const Projects = () => {
                     <h1 className="text-center">Projects</h1>
                 </div>
             </div>
+            
+            {projects.map((project) => {
+                return <Project key={uuidv4()} img={project.src} alt={project.alt} desc={project.desc}/>
+            })}
         </div>
     );
 };
